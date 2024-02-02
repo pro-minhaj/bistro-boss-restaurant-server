57a7abb0381da9066e6d839ebdef99f07d68413e
Md Minhaj <parsonal322532@gmail.com>
2024-02-02 17:50:57 +0600

    Code Modify

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ async function run() {
     // Payment APIS
     app.post("/create-payment-intent", VerifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
